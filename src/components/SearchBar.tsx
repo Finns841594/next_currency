358189dc87709fa1c49585fc4eccad484e74a712
Finns841594 <fengy142857@gmail.com>
2024-03-14 13:37:31 +0100

    setup nextui

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Card, CardBody } from '@nextui-org/react';
+
+function SearchBar() {
+  return (
+    <div>
+      <Card>
+        <CardBody>
+          <p>Make beautiful websites regardless of your design experience.</p>
+        </CardBody>
+      </Card>
+    </div>
+  );
+}
+
+export default SearchBar;
